1835c73ebbe8e1b6f5e95fb4597830b6ca425598
rodme02 <rodrigopm6@al.insper.edu.br>
2023-05-25 08:27:41 -0300

    backend pronto

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -4,3 +4,6 @@ from django.contrib.auth.models import User
 class Note(models.Model):
     cidade = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.cidade}"
